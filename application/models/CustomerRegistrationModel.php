c58e3d7735b24188398ef7e569291e5fb7aa8c18
ruwanliyanage123 <ruwanliyanagegalle1994@gmail.com>
2018-11-20 00:15:00 +0530

    add validateCustomers function

--- application/models/CustomerRegistrationModel.php
+++ application/models/CustomerRegistrationModel.php
@@ -5,10 +5,29 @@ class CustomerRegistrationModel extends CI_Model{
     }
 
     public function validateCustomers($username,$email, $password){
-        echo"ucsc";
-        echo $username;
-        echo $email;
-        echo $password;
+        //check that the customer is already registered or not 
+        $this->load->database();
+        $this->db->select('customer_email');
+        $this->db->from('customer');
+        $this->db->where('customer_email',$email);
+
+        $query = $this->db->get();
+        $row_number = $query->num_rows();
+
+        if($row_number!=0){
+            echo"already exists";
+        }
+
+        else{
+            $data = array(
+                'customer_name'     => $username,
+                'customer_email'    => $email,
+                'customer_password' => $password
+            );
+    
+            $this->db->set($data);
+            $this->db->insert($this->db->dbprefix.'customer');
+        }
     }
 }
 ?>
